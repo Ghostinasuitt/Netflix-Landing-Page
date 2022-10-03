ec28c605936ab1939aa2178812f1d7a869114605
Ghostinasuitt <michaelshomade@icloud.com>
2022-10-03 08:31:42 +0100

    Add border to tab. retrieve content from DOM. Add show class

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
 const tabItems = document.querySelectorAll(".tab-item");
-const tabContentItems = document.querySelectorAll(".tab-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
 
 //select tab content item
 function selectItem(e) {
   removeBorder();
-  //add border to current tab
+  removeShow();
+  // Add border to current tab
   this.classList.add("tab-border");
+  //  Grab content item from DOM
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  //   Add show class
+  tabContentItem.classList.add("show");
 }
 
 function removeBorder() {
   tabItems.forEach((item) => item.classList.remove("tab-border"));
 }
 
-//listen for tab click
+function removeShow() {
+  tabContentItems.forEach((item) => item.classList.remove("show"));
+}
+
+// listen for tab click
 tabItems.forEach((item) => item.addEventListener("click", selectItem));
